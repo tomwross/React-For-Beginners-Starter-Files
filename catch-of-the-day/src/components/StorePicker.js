1f91c84ecd8beb8c5abd3fdcd009da8fc8f830be
Tom Ross <tross@gadventures.com>
2019-01-02 14:35:10 -0500

    Update store picker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,16 +1,17 @@
 import React from "react";
 
 class StorePicker extends React.Component {
-    render() {
-        return (
-            <React.Fragment>
-                <p>Fish!</p>
-                <form className="store-selector">
-                    <h2>Please enter a store!</h2>
-                </form>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <form className="store-selector">
+          <h2>Please enter a store!</h2>
+          <input type="text" required placeholder="Store Name" />
+          <button type="submit">Visit Store --></button>
+        </form>
+      </React.Fragment>
+    );
+  }
 }
 
 export default StorePicker;
