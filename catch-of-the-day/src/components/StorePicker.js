3292c1daf23004ca92d9f4df8d830f037e31e930
Tom Ross <tross@gadventures.com>
2019-01-02 10:52:26 -0500

    Initial post-setup commit

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class StorePicker extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <p>Fish!</p>
+                <form className="store-selector">
+                    <h2>Please enter a store!</h2>
+                </form>
+            </React.Fragment>
+        );
+    }
+}
+
+export default StorePicker;
