6456bf37141bd49495a82f0d52b8bf6ca57085f3
Tom Ross <tross@gadventures.com>
2019-01-05 00:14:04 -0500

    Adding goToStore method to move to App component

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,12 +1,45 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  // constructor() {
+  //   super(); // runs React.Component constructor first, required
+  //   this.goToStore = this.goToStore.bind(this); // binds
+  // }
+
+  myInput = React.createRef();
+
+  goToStore = event => {
+    // event contains a lot of data and methods useful for controlling flow
+    //   // 1. Stop the form from submitting
+    event.preventDefault(); // stops the page from submitting
+    //   // 2. Get the text from that input
+    console.log(this.myInput);
+    //   // 3. Change the page to /store/whatever-store-name
+  };
+
+  // this method requires binding within constructor
+  // goToStore(event) {
+  //   // event contains a lot of data and methods useful for controlling flow
+  //   // 1. Stop the form from submitting
+  //   event.preventDefault(); // stops the page from submitting
+  //   // 2. Get the text from that input
+  //   console.log(this.myInput);
+  //   // 3. Change the page to /store/whatever-store-name
+  // }
+
   render() {
     return (
       <React.Fragment>
-        <form className="store-selector">
+        <form className="store-selector" onSubmit={this.goToStore}>
           <h2>Please enter a store!</h2>
-          <input type="text" required placeholder="Store Name" />
+          <input
+            type="text"
+            ref={this.myInput}
+            required
+            placeholder="Store Name"
+            defaultValue={getFunName()}
+          />
           <button type="submit">Visit Store --></button>
         </form>
       </React.Fragment>
