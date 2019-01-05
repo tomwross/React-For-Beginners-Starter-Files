9830f75b83a62d4c69f829db40c38c480123a543
Tom Ross <tross@gadventures.com>
2019-01-05 00:13:07 -0500

    Add Router component

---
+++ catch-of-the-day/src/components/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import StorePicker from "./StorePicker";
+import App from "./App";
+import NotFound from "./NotFound";
+
+// stateless functional component, arrow function style
+const Router = () => (
+  <BrowserRouter>
+    <Switch>
+      {/* routes go here */}
+      <Route exact path="/" component={StorePicker} />
+      <Route path="/store/:storeId" component={App} />
+      <Route component={NotFound} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Router;
