964e7a3b2f294a9cbcdfd058bf688723ebb7e9c0
Tom Ross <tross@gadventures.com>
2019-01-04 22:38:15 -0500

    Change header to stateless functional component

--- catch-of-the-day/src/components/Header.js
+++ catch-of-the-day/src/components/Header.js
@@ -1,23 +1,60 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">Of</span>
-            <span className="the">The</span>
-          </span>
-          Day
-        </h1>
-        <h3 className="tagline">
-          <span>{this.props.tagline}</span>
-        </h3>
-      </header>
-    );
-  }
-}
+// method 1 with normal function
+// function Header(props) {
+//   return (
+//     <header className="top">
+//       <h1>
+//         Catch
+//         <span className="ofThe">
+//           <span className="of">Of</span>
+//           <span className="the">The</span>
+//         </span>
+//         Day
+//       </h1>
+//       <h3 className="tagline">
+//         <span>{props.tagline}</span>
+//       </h3>
+//     </header>
+//   );
+// }
+
+// method 2 with arrow function and implicit return
+const Header = props => (
+  <header className="top">
+    <h1>
+      Catch
+      <span className="ofThe">
+        <span className="of">Of</span>
+        <span className="the">The</span>
+      </span>
+      Day
+    </h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
+
+// method 3 regular React component
+// class Header extends React.Component {
+//   render() {
+//     return (
+//       <header className="top">
+//         <h1>
+//           Catch
+//           <span className="ofThe">
+//             <span className="of">Of</span>
+//             <span className="the">The</span>
+//           </span>
+//           Day
+//         </h1>
+//         <h3 className="tagline">
+//           <span>{this.props.tagline}</span>
+//         </h3>
+//       </header>
+//     );
+//   }
+// }
 
 export default Header;
